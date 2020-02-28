1eca18db12399f539f57033963ee6703f4ea258b
Mark Stabler <mark@stablerwritingservices.com>
2020-02-28 21:21:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,11 +19,11 @@ address1 = "I LOVE YOU GRANDMA!"
 address2 = "Fuck off grandma!"
 address3 = "FUCK OFF GRANDMA!"
 
-# puts address1
-# speak_to_grandma(address1)
+puts address1
+puts speak_to_grandma(address1)
 
 # puts address2
-# speak_to_grandma(address2)
+# puts speak_to_grandma(address2)
 
-puts address3
-speak_to_grandma(address3)
+# puts address3
+# puts speak_to_grandma(address3)
